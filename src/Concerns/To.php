057b4577155bbbb80069c9e1bf25c0682a5e9476
ekadesh <122966905+ekadesh@users.noreply.github.com>
2023-06-26 14:33:38 +0300

    Update To.php
    Incompatible with CashierProvider\\Core\\Concerns\\Jsonable::toJson(int $options = 0): string

--- src/Concerns/To.php
+++ src/Concerns/To.php
@@ -6,7 +6,7 @@ namespace DragonCode\SimpleDataTransferObject\Concerns;
 
 trait To
 {
-    public function toJson($flags = JSON_UNESCAPED_UNICODE): string
+    public function toJson(int $flags = JSON_UNESCAPED_UNICODE): string
     {
         return json_encode($this->toArray(), $flags);
     }
